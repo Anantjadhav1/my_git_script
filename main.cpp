b16f2210b897546ab6bd0f9bc714932c73e9c011
Anantjadhav1 <anantjadhav2675@gmail.com>
2026-02-20 16:28:56 +0530

    added the sum of a and b

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main() {
     cout << "Hello GitHub! This is my first live C++ file." << endl;
+    int a = 12;
+    int b = 12;
+    cout<<"sum is "<<a+b;
     return 0;
 }
